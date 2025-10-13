35a7975b5e1668d4fe96dec0f0e21998f350f9fe
Thomas Galateau <thomas.galateau@gmail.com>
2025-10-13 16:46:21 +0200

    commit

--- tp_reduce_fetch_content/bench/main.cc
+++ tp_reduce_fetch_content/bench/main.cc
@@ -29,9 +29,9 @@ int main(int argc, char** argv)
         // Finally the true sizes
         // TODO
         constexpr std::array sizes = {
-            64
+            64,
             128,
-            129,
+            129
             // 524288,
             // 1048576
         };
