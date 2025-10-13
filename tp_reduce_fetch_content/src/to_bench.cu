77bab7d13a485d279820f39ca79b5538e89a85e6
Thomas Galateau <thomas.galateau@gmail.com>
2025-10-13 16:57:23 +0200

    commit

--- tp_reduce_fetch_content/src/to_bench.cu
+++ tp_reduce_fetch_content/src/to_bench.cu
@@ -73,7 +73,7 @@ void your_reduce(rmm::device_uvector<int>& buffer,
 
     rmm::device_uvector<int> tmp(3 * sizeof(int), buffer.stream());
 
-    kernel_your_reduce<int><<<3, 64, 3 * 64 * sizeof(int), buffer.stream()>>>(
+    kernel_your_reduce<int><<<512, 1024, 512 * 1024 * sizeof(int), buffer.stream()>>>(
         raft::device_span<const int>(buffer.data(), buffer.size()),
         raft::device_span<int>(tmp.data(), 1));
 
