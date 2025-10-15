7af4be0e66796b5398079e584b60609e45a286ef
Thomas Galateau <thomas.galateau@gmail.com>
2025-10-15 14:49:31 +0200

    commit

--- tp_reduce_fetch_content/src/to_bench.cu
+++ tp_reduce_fetch_content/src/to_bench.cu
@@ -50,7 +50,8 @@ void kernel_your_reduce(raft::device_span<const T> buffer, raft::device_span<T> 
     __syncthreads();
 
     for (int s = 1; s < blockDim.x; s *= 2) {
-        if (tid % (2 * s) == 0)
+        int index = 2 * s * tid;
+        if (index < blockDim.x)
             sdata[tid] += sdata[tid + s];
         __syncthreads();
     }
