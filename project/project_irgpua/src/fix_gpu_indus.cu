b88b976fb74bca61ae374e4ea1088d8f407bc68e
Thomas Galateau <thomas.galateau@gmail.com>
2025-11-13 15:34:49 +0100

    commit project

--- project/project_irgpua/src/fix_gpu_indus.cu
+++ project/project_irgpua/src/fix_gpu_indus.cu
@@ -9,13 +9,14 @@ struct NotGarbage {
     __device__ bool operator()(int val) const { return val != -27; }
 };
 
-void remove_garbage(raft::device_span<int> buffer, cudaStream_t stream)
+void remove_garbage(rmm::device_uvector<int> &buffer)
 {
     size_t n = buffer.size();
+    cudaStream_t stream = buffer.stream();
+    
     rmm::device_uvector<int> temp(n, stream);
     rmm::device_uvector<int> n_selected(1, stream);
     
-    // Première passe : calculer la taille du stockage temporaire
     void* d_temp_storage = nullptr;
     size_t temp_storage_bytes = 0;
     cub::DeviceSelect::If(
@@ -27,10 +28,7 @@ void remove_garbage(raft::device_span<int> buffer, cudaStream_t stream)
         stream
     );
     
-    // Allouer le stockage temporaire
     rmm::device_uvector<char> temp_storage(temp_storage_bytes, stream);
-    
-    // Deuxième passe : effectuer la sélection
     cub::DeviceSelect::If(
         temp_storage.data(), temp_storage_bytes,
         buffer.data(), temp.data(),
@@ -40,22 +38,50 @@ void remove_garbage(raft::device_span<int> buffer, cudaStream_t stream)
         stream
     );
     
-    // Obtenir le nombre d'éléments sélectionnés
     int num_selected = n_selected.element(0, stream);
-    printf("Number of garbage elements removed: %d\n", n - num_selected);
+    printf("Number of garbage elements removed: %d, should be 19595 for Image #0\n", n - num_selected);
     
-    // Copier SEULEMENT les éléments valides
     cudaMemcpyAsync(buffer.data(), temp.data(), num_selected * sizeof(int), 
                     cudaMemcpyDeviceToDevice, stream);
+    cudaStreamSynchronize(stream);
+    
+    buffer.resize(num_selected, stream);
+}
+
+__global__ void apply_pattern_kernel_optimized(raft::device_span<int> data, size_t n)
+{
+    const int adjustments[4] = {1, -5, 3, -8};
+    
+    size_t idx = blockIdx.x * blockDim.x + threadIdx.x;
+    
+    if (idx < n)
+    {
+        data[idx] += adjustments[idx % 4];
+    }
+}
+
+void apply_pattern_treatment(rmm::device_uvector<int> &buffer)
+{
+    size_t n = buffer.size();
+    cudaStream_t stream = buffer.stream();
+    
+    int threads_per_block = 256;
+    int num_blocks = (n + threads_per_block - 1) / threads_per_block;
+    
+    apply_pattern_kernel_optimized<<<num_blocks, threads_per_block, 0, stream>>>(
+        buffer.data(), n
+    );
+    
+    CUDA_CHECK_ERROR(cudaGetLastError());
 }
 
 void fix_image_gpu_indus(rmm::device_uvector<int> &buffer)
 {
-    size_t original_size = buffer.size();
-    remove_garbage(raft::device_span<int>(buffer.data(), buffer.size()), buffer.stream());
-    
-    // Note : Vous devrez redimensionner le buffer ou retourner la nouvelle taille
-    // buffer.resize(nouvelle_taille, buffer.stream()); 
+    remove_garbage(buffer);
+
+    CUDA_CHECK_ERROR(cudaStreamSynchronize(buffer.stream()));
+
+    apply_pattern_treatment(buffer);
     
     CUDA_CHECK_ERROR(cudaStreamSynchronize(buffer.stream()));
 }
