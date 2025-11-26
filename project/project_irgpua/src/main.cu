ec983ffb319a29f35a926be4e11bc0a139e14f40
Thomas Galateau <thomas.galateau@gmail.com>
2025-11-26 16:30:49 +0100

    commit project

--- project/project_irgpua/src/main.cu
+++ project/project_irgpua/src/main.cu
@@ -211,7 +211,7 @@ int main([[maybe_unused]] int argc, [[maybe_unused]] char* argv[])
         std::cout << "Image #" << images[i].to_sort.id << " total : " << images[i].to_sort.total << std::endl;
         if (images[i].to_sort.total + 1 != expected_images_total[i])
         {
-            std::cerr << "Image #" << images[i].to_sort.id << " error : total should be " << expected_images_total[i] << td::endl;
+            std::cerr << "Image #" << images[i].to_sort.id << " error : total should be " << expected_images_total[i] << std::endl;
         }
         std::ostringstream oss;
         oss << "Image#" << images[i].to_sort.id << ".pgm";
