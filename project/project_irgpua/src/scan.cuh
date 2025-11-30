88b885b89c8a8b169d4d92551b3b042da3943355
Théo Hénon <theo.henon@epita.fr>
2025-11-30 21:06:00 +0100

    fix(scan): allocate shared memory for reduce_block

--- project/project_irgpua/src/scan.cuh
+++ project/project_irgpua/src/scan.cuh
@@ -17,7 +17,8 @@ void exclusive_scan_byhand(rmm::device_vector<int> &d_data, rmm::device_vector<i
     cuda::std::span<int> data_span(thrust::raw_pointer_cast(d_data.data()), d_data.size());
     rmm::device_vector<int> reduced_blocks(GRID_SIZE);
     cuda::std::span<int> reduced_blocks_span(thrust::raw_pointer_cast(reduced_blocks.data()), reduced_blocks.size());
-    reduce_block<<<GRID_SIZE, BLOCK_SIZE>>>(data_span, reduced_blocks_span);
+    // Allocate dynamic shared memory for the block reduction (one int per thread)
+    reduce_block<<<GRID_SIZE, BLOCK_SIZE, BLOCK_SIZE * sizeof(int)>>>(data_span, reduced_blocks_span);
 
     rmm::device_vector<int> d_scanned_blocks(GRID_SIZE);
     cuda::std::span<int> scanned_blocks_span(thrust::raw_pointer_cast(d_scanned_blocks.data()), d_scanned_blocks.size());
